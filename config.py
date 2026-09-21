a7ab02f6bd12bc4924773315814b519bea767aac
ivegotanheadache <annachiaragargiulo11@gmail.com>
2026-09-21 17:48:39 +0200

    .

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-MAX_HYPONYMS = 5
-MAX_SYNONYMS = 2
-MAX_QUERY_ITERATIONS = 11
-MIN_CHAR_TOTAL = 0
-MAX_CHAR_TOTAL = 12
-EXPAND_MAX_DEPTH = 15
-EXTENDED = True
+MAX_HYPONYMS = 5   # Maximum number of hyponyms to add to the graph for each leaf
+MAX_SYNONYMS = 2   # Maximum number of synonyms to consider for each combination of words
+MAX_QUERY_ITERATIONS = 11   # Maximum number of iterations for each wikipedia/llm query
+MIN_CHAR_TOTAL = 0   # Minimum total number of characters for outputs final result
+MAX_CHAR_TOTAL = 12   # Maximum total number of characters outputs final result
+EXPAND_MAX_DEPTH = 15   # Maximum depth to expand the graph
+EXTENDED = True   # Whether to use extended graph expansion
 
 
 BACKEND = "openai"  # "openai" \ "llama"
